ee92e274c9ab6c1a53e8d0f0717e46b6c26466cc
cymbiotica <terry.esposito@gmail.com>
2018-10-13 14:17:24 -0400

    evilCoder

--- life.js
+++ life.js
@@ -1,27 +1,33 @@
-// TODO detemine how to populate ocolumnard so it doesnt end on turn 1 or do we
-
-let gameBoard = []
-const size = 4
-
-// createGameBoard = () => {
-//   for (let col = 0; col < size ; col++) {
-//     for (let row = 0; row < size; row++) {
-//         gameBoard[col].push(col, row)
-//     }        
-//   }
-//   return gameBoard
-// }
-
-
 createGameBoard = () => {
-  for (var i = 0; i < size; i++) {
-    gameBoard[i] = [];
-  }
-  return gameBoard;
+
 }
 
+gameboard = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
+checkBoard = () => {
+  let board = []
+  let arrToBoard = new Array(4).fill('cell')
+
+  for (let i = 0; i < arrToBoard.length; i++) {
+    board.push(arrToBoard)
+  }
+  return board
+}
+
+isCellAlive = () => {
+  return true
+}
+
+const cell = {
+  isAlive : () =>{
+    return true
+  }
+}
 
 module.exports = {
-  createGameBoard
+  createGameBoard,
+  gameboard,
+  checkBoard,
+  isCellAlive, 
+  cell
 }
--- test.js
+++ test.js
@@ -1,9 +1,20 @@
 const assert = require('assert')
 const life = require('./life.js')
 
-describe('createGameBoard()', () => {
-  it('should generate a board of 4 x 4 cells', () => {
-    assert.equal(life.createGameBoard, 16) // the ide is to count the cells?
+// describe('createBoard', () => {
+//   it('create a gameBoard of 4 x 4', () => {
+//     assert.equal(life.gameboard.length, 16)
+//   })
+// })
+
+describe('checkBoard', () => {
+  it('check to see if board is an array of 4 arrays with 4 elements in each ', () => {
+    assert.equal(life.checkBoard().length, 4)
   })
 })
 
+describe('isAlive', () => {
+  it('is this cell in an array alive', () => {
+    assert.equal(life.cell.isAlive(), true)
+  })
+})
