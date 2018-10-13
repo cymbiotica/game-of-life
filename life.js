ada98155ca35cfa21c3ed353e3c775067f58fb4e
cymbiotica <terry.esposito@gmail.com>
2018-10-13 14:45:15 -0400

    corey made the tests and I coded the answers

--- life.js
+++ life.js
@@ -1,33 +1,5 @@
-createGameBoard = () => {
-
-}
-
-gameboard = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-
-checkBoard = () => {
-  let board = []
-  let arrToBoard = new Array(4).fill('cell')
-
-  for (let i = 0; i < arrToBoard.length; i++) {
-    board.push(arrToBoard)
-  }
-  return board
-}
-
-isCellAlive = () => {
-  return true
-}
-
-const cell = {
-  isAlive : () =>{
-    return true
-  }
-}
+board = new Array(10).fill('x')
 
 module.exports = {
-  createGameBoard,
-  gameboard,
-  checkBoard,
-  isCellAlive, 
-  cell
+  board
 }
--- test.js
+++ test.js
@@ -1,20 +1,10 @@
 const assert = require('assert')
 const life = require('./life.js')
 
-// describe('createBoard', () => {
-//   it('create a gameBoard of 4 x 4', () => {
-//     assert.equal(life.gameboard.length, 16)
-//   })
-// })
+it('board is an array', () => {
+  assert.equal (life.board instanceof Array, true);
+});
 
-describe('checkBoard', () => {
-  it('check to see if board is an array of 4 arrays with 4 elements in each ', () => {
-    assert.equal(life.checkBoard().length, 4)
-  })
-})
-
-describe('isAlive', () => {
-  it('is this cell in an array alive', () => {
-    assert.equal(life.cell.isAlive(), true)
-  })
-})
+it('board array is equal to 10 spaces', () => {
+  assert.equal (life.board.length, 10);
+});
